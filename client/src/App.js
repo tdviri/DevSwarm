75c39fab7d47c8933e73dc30682d5dc02e7ef35e
Tom Dviri <tdviri20@gmail.com>
2024-04-24 12:55:02 -0400

    changes

--- client/src/App.js
+++ client/src/App.js
@@ -41,6 +41,7 @@ function App() {
   const [isShowingTaggedQuestions, setIsShowingTaggedQuestions] = useState(false);
   const [isLoggedIn, setIsLoggedIn] = useState(false);
   const [isGuest, setIsGuest] = useState(false);
+  const [loggedInUser, setLoggedInUser] = useState(null);
   let response;
 
   useEffect(()=> {
@@ -175,31 +176,7 @@ function App() {
   }
 
   return (
-    // <div className="body">
-    //   <Router>
-    //     <Routes>
-    //         {!isLoggedIn && !isGuest && (
-    //           <Route exact path="/" element={<Welcome setIsGuest={setIsGuest} setIsLoggedIn={setIsLoggedIn} users={users} />} />
-    //         )}
-  
-    //         <Route path="/login" element={<Login />} />
-    //         <Route path="/register" element={<Register />} />
-  
-    //         {(isLoggedIn || isGuest) && (
-    //           <Route path='/content' element={ <div>
-    //             <Navbar setCurrentTag={setCurrentTag} onSearch={setSearch} setIsNoQuestionsFound={setIsNoQuestionsFound} questions={questions} tags={tags} />
-    //             <div id="main" className="main">
-    //               <Sidebar toggleDisplayTagsPage={toggleDisplayTagsPage} />
-    //               <Forum setSearch={setSearch} displayTagsPage={displayTagsPage} setCurrentTag={setCurrentTag} toggleUnansweredBtnClicked={toggleUnansweredBtnClicked} setSortField={handleSort} addNewQuestion={addNewQuestion} fetchData={fetchData} answerPageIndex={answerPageIndex} displayAnswerForm={displayAnswerForm} displayAnswers={displayAnswers} showAnswerForm={showAnswerForm} handleAskQuestionBtn={handleAskQuestionBtn} isAskQuestionBtnClicked={isAskQuestionBtnClicked} setDisplayTagsPage={setDisplayTagsPage} tags={mapTags} ansArray={answers} setQuestions={setQuestions} handleAnswerPageIndex={handleAnswerPageIndex} isNoQuestionsFound={isNoQuestionsFound} questions={getSorted()} />
-    //             </div>
-    //           </div>
-    //          } />
-    //         )}
-    //     </Routes>
-    //   </Router>
-    // </div>
-    
-    <div className="body">
+  <div className="body">
     {!isLoggedIn && !isGuest && !showLoginPage && !showRegisterPage && (
       <Welcome setIsGuest={setIsGuest} setIsLoggedIn={setIsLoggedIn} users={users} />
     )}
